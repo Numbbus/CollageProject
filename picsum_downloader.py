57853bdade69551c014cbadb8eadc082f81aec8b
Vince <theros113@gmail.com>
2025-11-18 12:31:03 -0500

    Change how downloading images

--- picsum_downloader.py
+++ picsum_downloader.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import urllib.request
 import numpy as np
+import time
 
 def progress_bar(iteration, total, prefix='', suffix='', length=50, fill='█'):
     """
@@ -21,53 +22,61 @@ def progress_bar(iteration, total, prefix='', suffix='', length=50, fill='█'):
 
 
 number_of_images = 1000
-photo_resolution = 50
+photo_resolution = 250
 
 startingIndex = 0
 
 avgRgbValsOfAllImages = []
 
-outputPath = "testImages"
+outputPath = "images"
 
 url = f"https://picsum.photos/{photo_resolution}"
 
-for startingIndex in range(0,number_of_images):
+startingIndex = 0
 
-    urllib.request.urlretrieve(url, f"{outputPath}/picsumImg{startingIndex}.png")
+while startingIndex < number_of_images:
 
-    img = Image.open(f"{outputPath}/picsumImg{startingIndex}.png")
-    progress_bar(startingIndex+1, number_of_images, prefix='Downloading:',suffix='Complete',length=40)
-    #print(f"{startingIndex+1}/{number_of_images}")
+    try:
+        urllib.request.urlretrieve(url, f"{outputPath}/picsumImg{startingIndex}.png")
+        img = Image.open(f"{outputPath}/picsumImg{startingIndex}.png")
+        progress_bar(startingIndex+1, number_of_images, prefix='Downloading:',suffix='Complete',length=40)
+        #print(f"{startingIndex+1}/{number_of_images}")
 
-    #img.show()
+        #img.show()
 
-    with Image.open(f"{outputPath}/picsumImg{startingIndex}.png") as img:
-        img = img.convert('RGB')
+        with Image.open(f"{outputPath}/picsumImg{startingIndex}.png") as img:
+            img = img.convert('RGB')
 
-        # Retrieve the width and height of the image.
-        width, height = img.size
+            # Retrieve the width and height of the image.
+            width, height = img.size
 
-        # Initialize a list to store RGB values.
-        rgb_values = []
+            # Initialize a list to store RGB values.
+            rgb_values = []
 
-        # Use a list comprehension to populate the list with RGB values of each pixel.
-        rgb_values = [img.getpixel((x, y)) for y in range(height) for x in range(width)]
+            # Use a list comprehension to populate the list with RGB values of each pixel.
+            rgb_values = [img.getpixel((x, y)) for y in range(height) for x in range(width)]
 
-    averageRgbValues = (0, 0, 0)
-    totalRgbValues = (0, 0, 0)
+        averageRgbValues = (0, 0, 0)
+        totalRgbValues = (0, 0, 0)
 
-    counter = 0
+        counter = 0
 
-    for val in rgb_values:
-        totalRgbValues = (totalRgbValues[0] + val[0], totalRgbValues[1] + val[1], totalRgbValues[2] + val[2])
+        for val in rgb_values:
+            totalRgbValues = (totalRgbValues[0] + val[0], totalRgbValues[1] + val[1], totalRgbValues[2] + val[2])
 
-        counter+=1
+            counter+=1
 
-    averageRgbValues = (int(totalRgbValues[0] / counter), int(totalRgbValues[1] / counter), int(totalRgbValues[2] / counter))
+        averageRgbValues = (int(totalRgbValues[0] / counter), int(totalRgbValues[1] / counter), int(totalRgbValues[2] / counter))
 
-    avgRgbValsOfAllImages.append(averageRgbValues)
+        avgRgbValsOfAllImages.append(averageRgbValues)
 
-    #print(avgRgbValsOfAllImages)
+        #print(avgRgbValsOfAllImages)
+        startingIndex+=1
+    except:
+        time.sleep(1)
+        startingIndex-=1
+
+
 
 with open(f"{outputPath}/avg_rgb_values.txt", "w") as file:
     file.write(f"{avgRgbValsOfAllImages}")
